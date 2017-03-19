130f520edb110b794b0548db3fa7eec4868a49df
Tom Molesworth <TEAM@cpan.org>
2017-03-20 02:48:36 +0800

    Updated SSL handling

--- cpanfile
+++ cpanfile
@@ -3,6 +3,7 @@ requires 'Future', '>= 0.29';
 requires 'Carp', 0;
 requires 'IO::Async', '>= 0.62';
 requires 'Protocol::SOCKS', '>= 0.003';
+requires 'Syntax::Keyword::Try', 0;
 
 on 'test' => sub {
 	requires 'Test::More', '>= 0.98';
--- lib/Net/Async/SOCKS.pm
+++ lib/Net/Async/SOCKS.pm
@@ -29,8 +29,10 @@ Currently provides a very basic implementation of SOCKS_connect:
 use Carp qw(croak);
 use Protocol::SOCKS::Client;
 use Protocol::SOCKS::Constants qw(:all);
+use Syntax::Keyword::Try;
 use IO::Async::Loop;
 use IO::Async::Stream;
+use Log::Any qw($log);
 
 =head1 METHODS
 
@@ -71,7 +73,7 @@ sub IO::Async::Loop::SOCKS_connect {
 	my $on_socks_error = delete $params{on_socks_error} or defined wantarray or
 		croak "Expected 'on_socks_error' or to return a Future";
 
-	my $stream = delete $params{handle} || IO::Async::Stream->new;
+	my $stream = delete $params{handle} || IO::Async::Protocol::Stream->new;
 
 	$stream->isa( "IO::Async::Stream" ) or
 		croak "Can only SOCKS_connect a handle instance of IO::Async::Stream";
@@ -84,6 +86,7 @@ sub IO::Async::Loop::SOCKS_connect {
 	# in practice (ssh socks5) the endpoint could be blank
 	# * use this stream for TCP traffic
 	my $f;
+    $log->tracef("Establishing SOCKS connection to %s:%s", $socks_params{host}, $socks_params{port} || 1080);
 	$f = $loop->connect(
 		# The ->connect API has many ways of specifying
 		# the connection endpoint. More may be added in
@@ -95,60 +98,87 @@ sub IO::Async::Loop::SOCKS_connect {
 		service    => delete $socks_params{port} || 1080,
 	)->then(sub {
 		my ($sock) = @_;
+        try {
+            $log->tracef("We now have a sock (%s)", ref($sock));
 
-		# We're delegating most of the real work to the protocol here
-		my $proto = Protocol::SOCKS::Client->new(
-			version => 5,
-			writer => sub {
-				$sock->write(shift)
-			},
-			%socks_params,
-		);
+            # We're delegating most of the real work to the protocol here
+            my $proto = Protocol::SOCKS::Client->new(
+                version => 5,
+                writer => sub {
+                    $log->tracef("Writing [%s]", $_[0]);
+                    $sock->write(shift)
+                },
+                %socks_params,
+            );
 
-		# Proxy any read traffic into our protocol handler.
-		# Once we've negotiated the stream, we'll take over.
-		$stream->configure(
-			handle => $sock,
-			on_read => sub {
-				my ($stream, $buffref, $eof) = @_;
-				$proto->on_read($buffref);
-				if($eof) {
-					$stream->close;
-					$f->fail('connect' => 'something broke');
-				}
-			}
-		);
-		$loop->add($stream);
+            # Proxy any read traffic into our protocol handler.
+            # Once we've negotiated the stream, we'll take over.
+            $stream->configure(
+                handle => $sock,
+                on_read => sub {
+                    my ($stream, $buffref, $eof) = @_;
+                    $log->tracef("Reading [%s]", $$buffref);
+                    $proto->on_read($buffref);
+                    if($eof) {
+                        $stream->close;
+                        $f->fail('connect' => 'something broke');
+                    }
+                }
+            );
+            # $loop->add($stream);
 
-		# Version and auth header goes first
-		$stream->write($proto->init_packet);
+            # Version and auth header goes first
+            $log->tracef("Requesting init packet write");
+            $stream->write($proto->init_packet);
 
-		# Push the read handler - if we were
-		# given a prefab stream there may be a
-		# read handler in place already
-		# We're ready now - let's start the auth
-		# process.
-		$proto->auth(
-		)->then(sub {
-			$proto->connect(
-				ATYPE_IPV4,
-				$params{host},
-				$params{service},
-			)->transform(
-				done => sub {
-					$loop->remove($stream);
-					$stream
-				}
-			)
-		})
+            # Push the read handler - if we were
+            # given a prefab stream there may be a
+            # read handler in place already
+            # We're ready now - let's start the auth
+            # process.
+            return $proto->auth(
+            )->then(sub {
+                $log->tracef("Auth complete");
+                $proto->connect(
+                    ATYPE_IPV4,
+                    $params{host},
+                    $params{service},
+                )->transform(
+                    done => sub {
+                        $log->tracef("SOCKS protocol connection complete");
+                        # $loop->remove($stream);
+                        $stream
+                    }
+                )
+            })
+        } catch {
+            $log->errorf("Failure - %s", $@);
+            return Future->fail($@, 'socks');
+        }
 	});
 	$f->on_done($on_done) if $on_done;
 	$f->on_fail(sub {
+        $log->tracef("Error: %s", join ' ', @_);
 		$on_socks_error->($_[0]) if defined $_[1] and $_[1] eq "socks";
 	}) if $on_socks_error;
 
 	$f->on_ready(sub { undef $f });# unless defined wantarray;
-	return $f;
+	my %ssl_params = map { /^(SSL_.*)$/ ? ($1 => delete $params{$_}) : () } keys %params;
+    my $want_ssl = ($params{extensions} || [''])->[0] eq 'SSL';
+    $log->tracef("Want SSL, will upgrade") if $want_ssl;
+    return $f unless $want_ssl;
+	$f->then(sub {
+        my $stream = shift;
+        try {
+            $log->tracef("Upgrading to SSL with these options: %s", join ',', sort keys %ssl_params);
+            return $stream->SSL_upgrade(
+                %ssl_params
+            )
+        } catch {
+            $log->errorf("failed to upgrade - %s", $@);
+            return Future->fail($@, 'ssl');
+        }
+    });
 }
 
 1;
@@ -157,9 +187,9 @@ __END__
 
 =head1 AUTHOR
 
-Tom Molesworth <cpan@perlsite.co.uk>
+Tom Molesworth <TEAM@cpan.org>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2014. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2014-2015. Licensed under the same terms as Perl itself.
 
